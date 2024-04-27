ab2253bf3005abf7b1d173b7da3b47f43312c0cf
Nikita <68853033+Xplous@users.noreply.github.com>
2024-04-27 23:52:05 +0300

    Update 4.py

--- Task 1/4.py
+++ Task 1/4.py
@@ -1,12 +1,7 @@
 ship = "Б1В1Г1 Е4Е5 В4В5В6В7 Д3 Д6 З2К2"
-array = ship.split(" ")
 hod = input().upper()
-print(hod,array)
-for i in array:
-    if (hod in i):
-        print("Попал")
-        flag = True
-        break
+if (hod in ship):
+    print("Попал")
 else:
     print("Не попал")
 
