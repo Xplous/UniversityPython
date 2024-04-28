597078275808b8dfb904fcc465ffcefe645a1f60
Nikita <68853033+Xplous@users.noreply.github.com>
2024-04-28 11:31:51 +0300

    Update 4.py

--- Task 1/4.py
+++ Task 1/4.py
@@ -1,10 +1,10 @@
 ship = "Б1В1Г1 Е4Е5 В4В5В6В7 Д3 Д6 З2К2"
 hod = input().upper()
 try:
-    hod = int(input) 
+    hod = int(hod)
     print("Введите букву и цифру")
 except ValueError:
-       if (len(hod) == 2 & hod in ship):
+       if (len(hod) == 2 and hod in ship):
            print("Попал")
        else:
            print("Не попал")
