316b9680579caa522e0d2d59bb899b1d69481e8a
Nikita <68853033+Xplous@users.noreply.github.com>
2024-04-28 11:11:31 +0300

    Update 4.py

--- Task 1/4.py
+++ Task 1/4.py
@@ -1,9 +1,13 @@
 ship = "Б1В1Г1 Е4Е5 В4В5В6В7 Д3 Д6 З2К2"
 hod = input().upper()
-if (hod in ship):
-    print("Попал")
-else:
-    print("Не попал")
+try:
+    hod = int(input) 
+    print("Введите букву и цифру")
+except:
+    if (len(hod) == 2 && hod in ship):
+       print("Попал")
+    else:
+       print("Не попал")
 
 name,lastname,age = input(),input(),input()
 print("Ваше имя: {name} , Фамилия: {lastname}, Возраст: {age}".format(age=age, name=name, lastname=lastname))
